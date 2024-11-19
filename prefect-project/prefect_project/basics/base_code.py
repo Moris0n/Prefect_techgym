ff73dbcdf179a9ed89f21570fc22e6f3e2b42fd6
morsondak <mor@fraugster.com>
2024-11-19 11:11:49 +0100

    add some code

--- prefect-project/prefect_project/basics/base_code.py
+++ prefect-project/prefect_project/basics/base_code.py
@@ -1,25 +1,15 @@
 import time
-from prefect import flow, task
 
-# Tasks for demo
-@task
 def greet_user(name: str):
-    time.sleep(2)  # Simulating a delay
-    return f"Hello, {name}!\n"
-    
-@task
+    return f"Hello, {name}!\n"    
+
 def provide_status_update(name: str):
-    time.sleep(3)  # Simulating a delay
     return f"{name}, your current status is: Active\n"
     
-@task
 def fetch_account_balance(name: str):
-    time.sleep(5)  # Simulating a longer delay
     return f"{name}, your account balance is: $1,234.56\n"
 
-# Synchronous Flow
-@flow
-def user_notification_sync_flow(user_name: str):
+def user_account_flow(user_name:str):
     # Tasks run one after the other (synchronous execution)
     greeting_task = greet_user(user_name)
     status_task = provide_status_update(user_name)
@@ -29,8 +19,8 @@ def user_notification_sync_flow(user_name: str):
     final_message = greeting_task + status_task + balance_task
     return final_message
 
+
 if __name__ == "__main__":
-    # Synchronous execution demo
-    print("Synchronous Flow (Tasks run sequentially):")
-    result_sync = user_notification_sync_flow("John")
-    print(result_sync)
+    user_name = "Mor"
+    final_message = user_account_flow(user_name)
+    print(final_message)
--- prefect-project/prefect_project/basics/base_code_with_flows.py
+++ prefect-project/prefect_project/basics/base_code_with_flows.py
@@ -9,14 +9,18 @@ def provide_status_update(name: str):
 def fetch_account_balance(name: str):
     return f"{name}, your account balance is: $1,234.56\n"
 
-
-if __name__ == "__main__":
+def user_account_flow(user_name:str):
     # Tasks run one after the other (synchronous execution)
-    user_name  = "Mor"
     greeting_task = greet_user(user_name)
     status_task = provide_status_update(user_name)
     balance_task = fetch_account_balance(user_name)
 
     # Combine results sequentially
     final_message = greeting_task + status_task + balance_task
+    return final_message
+
+
+if __name__ == "__main__":
+    user_name = "Mor"
+    final_message = user_account_flow(user_name)
     print(final_message)
